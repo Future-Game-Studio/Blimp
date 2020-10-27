038e43a8287ff85aebf67404aac80e9aa880ad91
Long_as_Python <lahodniuk_ak19@nuwm.edu.ua>
2020-10-27 16:44:08 +0200

    dialogue system

---
+++ Game/Assets/Npc.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Npc : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject dialogue;
+
+    public void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed primary button.");
+            dialogue.SetActive(true);
+        }
+    }
+}
