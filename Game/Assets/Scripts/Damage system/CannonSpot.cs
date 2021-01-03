d27f2d74ef4e1e3c10a621e05eb87036e78d74d4
Danylo Lahodniuk <45416328+Long-as-Python@users.noreply.github.com>
2021-01-03 23:54:24 +0200

    Gun auto aim

--- Game/Assets/Scripts/Damage system/CannonSpot.cs
+++ Game/Assets/Scripts/Damage system/CannonSpot.cs
@@ -3,15 +3,37 @@ using UnityEngine;
 
 public class CannonSpot : GunManager
 {
+    private enum LookMode { Enemy, StartPos };
     public Gun gun;
     protected GameObject ChildGameObject;
     public float power;
 
+
     public void Start()
     {
-        GameObject bullet;
-
     }
+
+
+    Coroutine Look;
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Transform enemy = other.GetComponent<Transform>();
+            Look = StartCoroutine(LookAtEnemy(enemy, LookMode.Enemy));
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            StopCoroutine(Look);
+            ChildGameObject.transform.rotation = transform.rotation;
+            StartCoroutine(LookAtEnemy(transform, LookMode.StartPos));
+        }
+    }
+
     public void Shoot()
     {
         ChildGameObject = this.transform.GetChild(0).gameObject;
@@ -20,10 +42,10 @@ public class CannonSpot : GunManager
         Debug.DrawRay(transform.position, ChildGameObject.transform.forward, Color.yellow);
 #endif
         GameObject bullet;
-        bullet = Instantiate(gun.BulletPrefab, this.transform.position, Quaternion.identity);
+        bullet = Instantiate(gun.BulletPrefab, ChildGameObject.transform.position, Quaternion.identity);
         bullet.GetComponent<Bullet>().damage = gun.damage;
         Rigidbody rb = bullet.GetComponent<Rigidbody>();
-        rb.velocity = this.transform.forward * gun.power;
+        rb.velocity = ChildGameObject.transform.forward * gun.power;
         StartCoroutine(DestroyAfter(bullet, 4.0f));
     }
 
@@ -33,5 +55,31 @@ public class CannonSpot : GunManager
         Destroy(obj);
     }
 
+    private IEnumerator LookAtEnemy(Transform tr, LookMode lookMode)
+    {
+        ChildGameObject = this.transform.GetChild(0).gameObject;
+        while (true && lookMode == LookMode.Enemy)
+        {
+            var lookPos = tr.position - ChildGameObject.transform.position;
+            lookPos.y = 1f;
+            var rotation = Quaternion.LookRotation(lookPos);
+            ChildGameObject.transform.rotation = Quaternion.Slerp(ChildGameObject.transform.rotation, rotation, Time.deltaTime);
+            yield return null;
+        }
+        while (true && lookMode == LookMode.StartPos)
+        {
+            var lookPos = tr.position - ChildGameObject.transform.position;
+            lookPos.y = 1f;
+            lookPos.x = 0f;
+            lookPos.z = 0f;
+            var rotation = Quaternion.LookRotation(lookPos);
+            ChildGameObject.transform.rotation = Quaternion.Slerp(ChildGameObject.transform.rotation, rotation, Time.deltaTime);
 
+            if (ChildGameObject.transform.rotation == transform.rotation)
+            {
+                break;
+            }
+            yield return null;
+        }
+    }
 }
